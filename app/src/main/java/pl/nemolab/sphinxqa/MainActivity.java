03e5d6616d884fa85f37afb905ec8b047e15fb25
senator <radoslaw.drewa@gmail.com>
2015-01-18 01:43:07 +0100

    Finding files which can be movies

--- app/src/main/java/pl/nemolab/sphinxqa/MainActivity.java
+++ app/src/main/java/pl/nemolab/sphinxqa/MainActivity.java
@@ -3,14 +3,23 @@ package pl.nemolab.sphinxqa;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,6 +27,8 @@ public class MainActivity extends ActionBarActivity {
     private EditText edtVideoFile, edtSrcFile, edtDstFile;
     private TextView txtVideoPath;
     private Button btnPlay;
+    private List<String> listVideoFiles;
+    private List<String> listVideoPaths;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,11 +76,33 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void showPickVideoDialog() {
+        String[] columns = {
+                MediaStore.Video.VideoColumns.TITLE,
+                MediaStore.Video.VideoColumns.DATA,
+        };
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Video.VideoColumns.SIZE + " > ? AND "
+                + MediaStore.Video.VideoColumns.DURATION + " > ?";
+        String[]  params = {"100000000", "1200000"};
+        String orderBy = MediaStore.Video.VideoColumns.TITLE + " ASC";
+        Cursor cursor = getContentResolver().query(uri, columns, selection, params, orderBy);
+        listVideoFiles = new ArrayList<>();
+        listVideoPaths = new ArrayList<>();
+        if (cursor != null && cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                listVideoFiles.add(cursor.getString(0));
+                listVideoPaths.add(cursor.getString(1));
+            }
+        }
+        String[] arrVideoFiles = listVideoFiles.toArray(new String[listVideoFiles.size()]);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Pick a video file");
-        builder.setItems(new CharSequence[] {"asdfa", "afsda"}, new DialogInterface.OnClickListener() {
+        builder.setItems(arrVideoFiles, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                String msg = "Title: " + listVideoFiles.get(which) + "\n"
+                        + "Path: " + listVideoPaths.get(which);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
             }
         });
         Dialog dialog = builder.create();
