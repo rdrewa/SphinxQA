b6fca9661c2c134c9992be45bc8c270fd31e6f0e
senator <radoslaw.drewa@gmail.com>
2015-02-20 15:18:19 +0100

    Removed unused menus

--- app/src/main/java/pl/nemolab/sphinxqa/gui/EditActivity.java
+++ app/src/main/java/pl/nemolab/sphinxqa/gui/EditActivity.java
@@ -67,26 +67,4 @@ public class EditActivity extends ActionBarActivity {
             position = bundle.getInt(MarkedActivity.POSITION);
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/pl/nemolab/sphinxqa/gui/MergeActivity.java
+++ app/src/main/java/pl/nemolab/sphinxqa/gui/MergeActivity.java
@@ -130,27 +130,4 @@ public class MergeActivity extends ActionBarActivity {
         setResult(RESULT_OK, data);
         finish();
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_merge, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
